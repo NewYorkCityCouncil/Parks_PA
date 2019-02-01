4079cf6cbfbcb83b55ad2d79d20503e9ea1db717
Nick Solomon <nsolomon@council.nyc.gov>
2019-02-01 10:26:15 -0500

    House cleaning

--- code/events.R
+++ code/events.R
@@ -1,121 +1,74 @@
-library(rvest)
-library(stringr)
+library(tidyverse)
+library(jsonlite)
+library(sf)
+library(leaflet)
+library(councildown)
+library(lubridate)
+# library(timevis)
+library(htmltools)
+library(htmlwidgets)
 
+source(here::here("code", "util.R"))
 
+events_raw <- fromJSON("https://www.nycgovparks.org/xml/events_300_rss.json") %>%
+  # as_tibble() %>%
+  # mutate(coords = map(coordinates, ~as_tibble(str_split(., ";", simplify = TRUE))),
+  #        coords = map(coords, ~separate(., 1, c("lat", "lng"), sep = ", ", convert = TRUE))) %>%
+  # unnest(coords) %>%
+  # st_as_sf(coords = c("lng", "lat"), crs = "+proj=longlat +datum=WGS84") %>%
+  identity()
 
-baseurl <- 'https://www.nycgovparks.org'
+# sum(duplicated(events_raw$geometry))
 
-pages_add <- '/events/p'
+# make_caption <- function(dat) {
+#   window_start <- as_datetime(dat$start[1] - 30*60)
+#   window_end <- as_datetime(dat$end[1] + 30*60)
+#   dat %>%
+#     dplyr::select(description, start, end) %>%
+#     rename(content = description) %>%
+#     # mutate(id = row_number()) %>%
+#     timevis::timevis() %>%
+#     # setWindow(window_start, window_end) %>%
+#     identity()
+#   # print(window_start)
+#   # print(window_end)
+# }
 
-page_count <- as.character(seq(1,3))
-
-park_events <- data.frame(event = character(), location = character(), month = character(), 
-                          day = integer(), start_time = character(), end_time = character(), desc = character())
-
-data.frame(event = character(), park = character(), location = character(),
-           start_time = character(),end_time = character(), accessible = character())
-
-
-for (i in 1:length(page_count)){
-  page = read_html(paste(baseurl,pages_add,page_count[[i]], sep = ''))
-  urls <- page %>% 
-    html_nodes(".event-title a") %>% 
-    html_attr('href')
-  month <- page %>%
-    html_nodes('.cal_month') %>%
-    html_text()
-  day <- page %>%
-    html_nodes('.cal_day') %>%
-    html_text() %>%
-    as.numeric()
-  location <- page %>%
-    html_nodes('.location') %>%
-    html_text()
-  categories <- page %>%
-    html_nodes("#events_leftcol span a , strong:nth-child(6)") %>%
-    html_text() %>%
-    paste(., collapse = ", ") %>%
-    strsplit(., 'Category: ')%>%
-    unlist(.)
-  categories <- categories[categories != '']
-  start_time <- page %>%
-    html_nodes('#events_leftcol strong:nth-child(1)') %>%
-    html_text()
-  end_time <- page %>%
-    html_nodes('strong:nth-child(2)') %>%
-    html_text()
-  desc <- page %>%
-    html_nodes('.description') %>%
-    html_text()
-  event <- page %>%
-    html_nodes('.event-title a') %>%
-    html_text()
-  
-  new_events_row <- data.frame(event, location, month, day, start_time, end_time, desc)
-  park_events <- rbind(park_events, new_events_row)
+make_caption <- function(dat) {
+  dat <- dat %>%
+    mutate(cap = paste("<h4>", title, "</h4>", description))
+  out <- paste(dat$cap, collapse = "<hr>")
+  out
 }
 
-
-df
-
-df <- 0
-df <- data.frame(event = character(), park = character(), location = character(),
-                 start_time = character(),end_time = character(), accessible = character())
-for (i in 1:length(urls)){
-  webpage <- read_html(paste(baseurl,urls[[i]], sep = ''))
-  event_org <- webpage %>%
-    html_nodes('p:nth-child(9)') %>%
-    html_text()
-  park <- webpage %>%
-    html_nodes('.location div a') %>%
-    html_text()
-  accessible <- webpage %>%
-    html_nodes('#single_event_body img') %>%
-    html_attr('src')
-  if (!is.null(accessible)){
-    accessible <- 'yes'
-  } else { accessible <- 'no'}
-  newrow <- data.frame(event, park, location,start_time,end_time, accessible)
-  df <- rbind(df, newrow)
-  #print(c(event,park, location, start_time, end_time, event_org, accessible))
-  i <- i+1
-  }
+events <- events_raw %>%
+  unite(start, starts_with("start"), sep = " ") %>%
+  unite(end, starts_with("end"), sep = "") %>%
+  mutate(start = ymd_hm(start),
+         end = ymd_hm(end)) %>%
+  group_by(coordinates) %>%
+  nest() %>%
+  mutate(caption = map_chr(data, make_caption)) %>%
+  mutate(coords = map(coordinates, ~as_tibble(str_split(., ";", simplify = TRUE))),
+         coords = map(coords, ~separate(., 1, c("lat", "lng"), sep = ", ", convert = TRUE))) %>%
+  unnest(coords) %>%
+  st_as_sf(coords = c("lng", "lat"), crs = "+proj=longlat +datum=WGS84") %>%
+  identity()
 
 
+events_map <- leaflet(events) %>%
+  addCouncilStyle() %>%
+  addCircleMarkers(radius = 4,
+                   color = "#F59F00",
+                   popup = ~councilPopup(caption), popupOptions = popupOptions(maxHeight = 250), fillOpacity = .8,
+                   stroke = FALSE) %>%
+  setView(-73.88099670410158,40.72540497175607,  zoom = 10.5)%>%
+  registerPlugin(geocoder) %>%
+  # registerPlugin(fontawsome_markers) %>%
+  onRender(geocode_js, data = list(key = Sys.getenv("GEOCODE_API_KEY")))
 
-df
+htmlwidgets::saveWidget(events_map, file = "events_map.html", selfcontained = FALSE)
+unlink(here::here("results", "events_map_files"), recursive = TRUE)
+file.rename("events_map.html", "results/events_map.html")
+file.rename("events_map_files", "results/events_map_files")
 
-
-
-
-tempo <- read_html('https://www.nycgovparks.org/events/2019/01/09/6th-annual-whimsical-winter-wonder-exhibition')
-
-
-
-
-df <- data.frame(event, park, location, event_org, accessible, phone, email, start_time, end_time, accessible)
-df
-
-length(free)
-
-for (i in 1:length(free)) {
-  print(free[[i]])}
-
-
-i <- 13
-for (i in 1:length(categories)) {
-  if (categories[[i]] == "Category: "){
-    if (categories[[i+1]] == "Free!") {
-      print(categories[[i]])
-    }
-  } else if (categories[[i]] == "Category: "){
-    if (categories[[i+1]] == "Category: ") {
-      print(categories[[i]])
-    }
-  } else if (categories[[i]] != "Category: ") {
-      print(categories[[i]])
-  } else {
-    print("OH NO")
-  }
-    i=i+1
-}
