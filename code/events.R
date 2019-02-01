238ad55c0236833263e6ec53d940eb7170948c41
Nick Solomon <nsolomon@council.nyc.gov>
2019-02-01 16:26:20 -0500

    Write tests

--- code/events.R
+++ code/events.R
@@ -7,6 +7,8 @@ library(lubridate)
 # library(timevis)
 library(htmltools)
 library(htmlwidgets)
+library(testthat)
+
 
 source(here::here("code", "util.R"))
 
@@ -36,16 +38,25 @@ events_raw <- fromJSON("https://www.nycgovparks.org/xml/events_300_rss.json") %>
 
 make_caption <- function(dat) {
   dat <- dat %>%
+    # Order by date
     mutate(cap = paste("<h4>", title, "</h4>", description))
   out <- paste(dat$cap, collapse = "<hr>")
   out
 }
 
+expect_is(events_raw, "data.frame", info = "Raw event data is not a data frame.")
+expect_gt(nrow(events_raw), 0, info = "No event data downloaded")
+expect_is(events_raw$description, "character", info = "Description is not text data")
+expect_is(events_raw$coordinates, "character", info = "Coordinates are not text data")
+expect_equal(sum(is.na(events_raw$coordinates)), 0, info = "Missing coordinates")
+expect_equal(sum(is.na(events_raw$description)), 0, info = "Missing descriptions")
+
+
 events <- events_raw %>%
-  unite(start, starts_with("start"), sep = " ") %>%
-  unite(end, starts_with("end"), sep = "") %>%
-  mutate(start = ymd_hm(start),
-         end = ymd_hm(end)) %>%
+  # unite(start, starts_with("start"), sep = " ") %>%
+  # unite(end, starts_with("end"), sep = "") %>%
+  # mutate(start = ymd_hm(start),
+  #        end = ymd_hm(end)) %>%
   group_by(coordinates) %>%
   nest() %>%
   mutate(caption = map_chr(data, make_caption)) %>%
@@ -55,6 +66,10 @@ events <- events_raw %>%
   st_as_sf(coords = c("lng", "lat"), crs = "+proj=longlat +datum=WGS84") %>%
   identity()
 
+expect_is(events, "sf", info = "Events not spatial data")
+expect_equal(sum(is.na(events$geometry)), 0, info = "Missing points")
+expect_is(events$caption, "character", info = "Generated caption is not text data")
+expect_equal(sum(is.na(events$caption)), 0, info = "Missing captions")
 
 events_map <- leaflet(events) %>%
   addCouncilStyle() %>%
@@ -67,8 +82,17 @@ events_map <- leaflet(events) %>%
   # registerPlugin(fontawsome_markers) %>%
   onRender(geocode_js, data = list(key = Sys.getenv("GEOCODE_API_KEY")))
 
-htmlwidgets::saveWidget(events_map, file = "events_map.html", selfcontained = FALSE)
-unlink(here::here("results", "events_map_files"), recursive = TRUE)
-file.rename("events_map.html", "results/events_map.html")
-file.rename("events_map_files", "results/events_map_files")
+time <- Sys.time()
 
+htmlwidgets::saveWidget(events_map, file = "events_map.html", selfcontained = FALSE)
+
+expect_true(file.mtime("events_map.html") >= time, info = "events_map.html was not properly saved")
+expect_true(file.mtime("events_map_files") >= time, info = "events_map_files was not properly saved")
+
+delete <- unlink(here::here("results", "events_map_files"), recursive = TRUE)
+move1 <- file.rename("events_map.html", "results/events_map.html")
+move2 <- file.rename("events_map_files", "results/events_map_files")
+
+expect_equal(delete, 0, info = "Could not delete old JS dependencies")
+expect_true(move1, info = "Could not move events_map.html to results/")
+expect_true(move2, info = "Could not move events_map_files/ to results/")
