0e58e91a0786101f39b0982c9b964e9b19d22e88
Nick Solomon <nsolomon@council.nyc.gov>
2019-02-20 17:15:11 -0500

    show tomorrow's events

--- code/events.R
+++ code/events.R
@@ -124,6 +124,7 @@ events_out <- events_raw %>%
          end = ymd_hm(end),
          pretty_time = format(start, format = "%b %d %I:%M %p")) %>%
   arrange(start) %>%
+  filter(start > Sys.Date() + 1) %>%
   head(5)
 
 write_json(events_out, "results/data_files/events.json")
